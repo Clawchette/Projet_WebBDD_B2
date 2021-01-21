7cd171d4ce2a33fc93e618fa3259e61e09264789
Clawchette <marie.gonzalez@ynov.com>
2021-01-21 10:40:55 +0100

    css du header et footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,17 +7,14 @@
     <meta name="description" content="Achat de jeux vidéos en ligne">
     <meta name="author" content="GONZALEZ Marie, ALARD Adrien">
     <title>eGames</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
     @include('header')
-
+    
     @yield('contenu')
-    <div class="container">
-
-    </div>
 
     @include('footer')
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
---
+++ storage/framework/views/5df84a1b32d42d9c4dab097f9395b31d8c32cc7d.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>© 2021 eGames</p>
+</footer><?php /**PATH C:\Users\Clawc\Documents\B2-informatique\Projet_Web\Projet_WebBDD_B2\resources\views/footer.blade.php ENDPATH**/ ?>
