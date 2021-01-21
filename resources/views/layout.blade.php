eaa1a08412709bc99f90026bd3e02fe2b3f403c1
tankplasma <adrien.alard@gmail.com>
2021-01-21 10:49:49 +0100

    test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,8 +7,9 @@
     <meta name="description" content="Achat de jeux vidéos en ligne">
     <meta name="author" content="GONZALEZ Marie, ALARD Adrien">
     <title>eGames</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,17 @@
 @extends('layout')
 
 @section('contenu')
-
-
+    <div class="principale">
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+        <div class="jeu_photo"></div>
+    </div>
 
 @endsection
